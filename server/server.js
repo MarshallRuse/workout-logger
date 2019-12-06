1f58151a239203ebb9e57c90bb129ec0cbfd7cb9
Marshall Ruse <12mr9@queensu.ca>
2019-12-05 20:31:53 -0500

    Added build directory routing for server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+
+app.use(cors());
 
 const publicPath = path.join(__dirname, '..', 'public');
 
 
-app.use(express.static(publicPath));
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.resolve(__dirname, '..', 'build')));
+} else {
+    app.use(express.static(publicPath));
+}
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
-})
-
+if (process.env.NODE_ENV === 'production'){
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+    });
+} else{
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(publicPath, 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
